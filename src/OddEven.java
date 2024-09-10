48394c3cca7dcd4fdbf8cb8b7533da9e477f006f
rajesh_purella <122461906+rajesh-purella@users.noreply.github.com>
2024-09-10 13:33:29 +0530

    Initial commit

---
+++ src/OddEven.java
@@ -0,0 +1,15 @@
+
+public class OddEven {
+
+    public static void main(String [] args)
+    {
+        int a = 5;
+        if(a%2==0)
+        {
+            System.out.println(a+" is a prime nummber");
+        }
+        else {
+            System.out.println(a+" not a prime number");
+        }
+    }
+}
